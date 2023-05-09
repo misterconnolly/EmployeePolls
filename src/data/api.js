c2b1be2aade29289775454e1af021871ca31a563
Brian Connolly <brian@thingsoutofstuff.com>
2023-05-09 13:42:22 -0700

    Cleanup starter project

---
+++ src/data/api.js
@@ -0,0 +1,15 @@
+export const _getUsers = async () => {
+
+};
+
+export const _getQuestions = async () => {
+
+};
+
+export const _saveQuestion = async ({author, optionOneText, optionTwoText}) => {
+
+};
+
+export const _saveQuestionAnswer = async ({authedUser, qid, answer}) => {
+
+};
