0ff07e9b7e1d2fede036d8dd1bf3020534f3a011
Brian Connolly <brian@thingsoutofstuff.com>
2023-05-10 14:49:35 -0700

    Cleanup tests

--- src/data/api.test.js
+++ src/data/api.test.js
@@ -13,32 +13,5 @@ describe("getUsers", () => {
   });
 });
 
-// describe("getQuestions", () => {
-//     it("should return a promise", () => {
-//       const result = getQuestions();
-//       expect(result).toBeInstanceOf(Promise);
-//     });
-  
-//     it("should return list of questions", async () => {
-//       const result = await getQuestions ();
-//       expect(Object.keys(result).length).toBeGreaterThan(0);
-//     });
-//   });
-
-
-// describe("saveAnswer", () => {
-//     const authedUser = makeStringOfLength(10);
-//     const qid = makeStringOfLength(10);
-//     const answer = makeStringOfLength(10);
-
-//     it("should return a promise", () => {
-//       const result = saveAnswer({
-//         authedUser,
-//         qid,
-//         answer
-//       });
-//       expect(result).toBeInstanceOf(Promise);
-//     });
-//   });
 
 
