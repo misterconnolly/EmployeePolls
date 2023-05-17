a89ce32d8fbd2a5c12129d2addcc36eed583f262
Brian Connolly <brian@thingsoutofstuff.com>
2023-05-16 23:35:03 -0700

    Add question to array on user in store

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,6 +1,7 @@
 import { ANSWER_QUESTION } from "../actions/answers";
 import { RECEIVE_DATA } from "../actions/shared";
 import { ADD_USER } from "../actions/users";
+import { ADD_QUESTION } from "../actions/questions";
 
 export default function users(state = [], action) {
   switch (action.type) {
@@ -9,17 +10,28 @@ export default function users(state = [], action) {
         ...state,
         [action.user.id]: action.user
       }
-      case ANSWER_QUESTION:
-        return {
-          ...state,
-          [action.authedUser]: {
-            ...state[action.authedUser],
-            answers: {
-              ...state[action.authedUser].answers,
-              [action.qid]: action.answer
-            }
+    case ADD_QUESTION:
+      return {
+        ...state,
+        [action.question.author]: {
+          ...state[action.question.author],
+          questions: [
+            ...state[action.question.author].questions,
+            action.question.id
+          ]
+        }
+      }      
+    case ANSWER_QUESTION:
+      return {
+        ...state,
+        [action.authedUser]: {
+          ...state[action.authedUser],
+          answers: {
+            ...state[action.authedUser].answers,
+            [action.qid]: action.answer
           }
         }
+      }
     case RECEIVE_DATA:
       return action.users;
     default:
