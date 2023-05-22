26785c2c38e78a8120fd0c9fb53568519d52e6da
Brian Connolly <brian@thingsoutofstuff.com>
2023-05-22 16:00:50 -0700

    Toggle for question answer

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,12 +3,13 @@ import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 import { handleAnswerQuestion } from "../actions/answers";
 import { createAvatarUrlIfEmpty } from "../util/avatar";
-import { Container, Row, Col, Button, Card } from "react-bootstrap";
+import { Container, Row, Col, Button, Card, ButtonGroup, ToggleButton } from "react-bootstrap";
 
 const Question = (props) => {
     const { id } = useParams();
     const [question, setQuestion] = useState(null);
     const [author, setAuthor] = useState(null);
+    const [answer, setAnswer] = useState("");
 
     useEffect(() => {
         setQuestion(props.questions[id]);
@@ -17,40 +18,75 @@ const Question = (props) => {
     useEffect(() => {
         if (props.questions[id] && props.questions[id].author)
             setAuthor(props.users[props.questions[id].author])
-    }, [props.users, props.questions, id]);    
+    }, [props.users, props.questions, id]); 
+    
+    useEffect(() => {
+        if (props.users[props.authedUser.id].answers[id])
+            setAnswer(props.users[props.authedUser.id].answers[id])
+    }, [props.users, props.questions, id, props.authedUser.id]); 
+    
 
-    const handleOptionOne = (e) => {
+
+    const handleSelect = (e) => {
         props.dispatch(handleAnswerQuestion({
             authedUser: props.authedUser.id,
             qid: id, 
-            answer: 'optionOne'}));
+            answer: e.target.value}));
     };
 
-    const handleOptionTwo = (e) => {
-        props.dispatch(handleAnswerQuestion({
-            authedUser: props.authedUser.id,
-            qid: id, 
-            answer: 'optionTwo'}));
-    };
 
     return (
-        <div className="question-card">
+      <div className="question-card">
         {question && author && props.authedUser && (
-            <Card className="text-center">
-                <Card.Header>Poll by {author.id}</Card.Header>
-                <Card.Body>
-                    <img src={createAvatarUrlIfEmpty(author.avatarURL, author.name)} alt="User avatar" className="avatar-max-width-20p" />
-                    <Card.Title>Would You Rather?</Card.Title>
-                    <Container fluid>
-                        <Row>
-                            <Col><Button variant="primary" onClick={handleOptionOne}>{question.optionOne.text}</Button></Col>
-                            <Col><Button variant="primary" onClick={handleOptionTwo}>{question.optionTwo.text}</Button></Col>
-                        </Row>
-                    </Container>
-                </Card.Body>
-            </Card>
+          <Card className="text-center">
+            <Card.Header>Poll by {author.id}</Card.Header>
+            <Card.Body>
+              <img
+                src={createAvatarUrlIfEmpty(author.avatarURL, author.name)}
+                alt="User avatar"
+                className="avatar-max-width-20p"
+              />
+              <Card.Title>Would You Rather?</Card.Title>
+              <Container fluid>
+                <Row>
+                  <ButtonGroup>
+                    <Col>
+                      <ToggleButton
+                        key="optionOne"
+                        id={`radio-optionOne`}
+                        type="radio"
+                        variant={'outline-primary'}
+                        name="radio"
+                        value="optionOne"
+                        checked={answer === "optionOne"}
+                        onChange={handleSelect}
+                      >
+                        {question.optionOne.text}
+                      </ToggleButton>
+                    </Col>
+                    <Col>
+                      <ToggleButton
+                        key="optionTwo"
+                        id={`radio-optionTwo`}
+                        type="radio"
+                        variant={'outline-primary'}
+                        name="radio"
+                        value="optionTwo"
+                        checked={answer === "optionTwo"}
+                        onChange={handleSelect}
+                      >
+                        {question.optionTwo.text}
+                      </ToggleButton>
+
+                    </Col>
+                  </ButtonGroup>
+                </Row>
+              </Container>
+
+            </Card.Body>
+          </Card>
         )}
-        </div>
+      </div>
     );
 };
 
